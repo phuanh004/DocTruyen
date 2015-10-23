41c2cd36f7f8ab07381b237b7ae93f09529f17ab
phuanh004 <phuanh004@gmail.com>
2015-10-23 17:15:28 +0700

    Update

--- app/src/main/java/com/anhpppd01336/doctruyen/DocTruyenOnlineActivity.java
+++ app/src/main/java/com/anhpppd01336/doctruyen/DocTruyenOnlineActivity.java
@@ -7,12 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class DocTruyenActivity extends AppCompatActivity {
+public class DocTruyenOnlineActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_doc_truyen);
+        setContentView(R.layout.activity_doc_truyen_online);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
